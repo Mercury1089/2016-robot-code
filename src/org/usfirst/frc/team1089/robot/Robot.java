437b435376db281594b7f038a0e247e644a0e344
Mercury 1089 <Mercury 1089@172.16.197.101>
2016-01-19 18:58:25 -0800

    Camera Network

--- src/org/usfirst/frc/team1089/robot/Robot.java
+++ src/org/usfirst/frc/team1089/robot/Robot.java
@@ -3,23 +3,45 @@
 package org.usfirst.frc.team1089.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
+	NetworkTable nt;
+	double[] rectWidth, rectHeight, rectCenterX, rectCenterY;
+	
 	public void robotInit() {
-
-    }
+	
+	}
 
 	public void autonomousPeriodic() {
 
     }
 
     public void teleopPeriodic() {
-        
+    	nt = NetworkTable.getTable("GRIP/myContoursReport");
+    	   
+    	getNTInfo();
+        debug();
     }
     
     public void testPeriodic() {
     
     }
+
+    private void getNTInfo() {
+    	double[] iv = {-1};
+    	
+    	rectWidth = nt.getNumberArray("width", iv);
+    	rectHeight = nt.getNumberArray("height", iv);
+    	rectCenterX = nt.getNumberArray("centerX", iv);
+    	rectCenterY = nt.getNumberArray("centerY", iv);
+    	
+    }
     
+    private void debug() {
+    	SmartDashboard.putString("Rect Center", "(" + rectCenterX[0] + ", " + rectCenterY[0] + ")");
+    	SmartDashboard.putString("Rect Size", "(" + rectWidth[0] + ", " + rectHeight[0] + ")");
+    }
 }
