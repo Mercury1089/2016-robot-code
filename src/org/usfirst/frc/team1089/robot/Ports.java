2fde79718180b61058b5f499ce9b239c25e51523
Mercury 1089 <Mercury 1089@172.16.197.110>
2016-01-21 17:18:48 -0800

    Fixed Ports and enhanced vision

--- src/org/usfirst/frc/team1089/robot/Ports.java
+++ src/org/usfirst/frc/team1089/robot/Ports.java
@@ -1,1 +1,31 @@
+package org.usfirst.frc.team1089.robot;
+
+public class Ports {
+	
+
+	public static class Analog {
+	
+	}
+
+	public static class Digital {
+		
+	}
+
+	public static class PWM {
+		public static final int LEFT_TALON = 0;
+		public static final int RIGHT_TALON = 1;
+		
+	}
+
+	public static class USB {
+		public static final int	LEFT_STICK	= 0;
+		public static final int	RIGHT_STICK	= 1;
+
+		public static final int	GAMEPAD		= 2;
+	}
+
+	public static class PCM {
+	
+	}
+}
 
--- src/org/usfirst/frc/team1089/robot/Robot.java
+++ src/org/usfirst/frc/team1089/robot/Robot.java
@@ -6,26 +6,40 @@ import java.util.Arrays;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 
 	// Deploy NetworkTable to roboRIO
-	NetworkTable nt;
-
+	private NetworkTable nt;
+	private double largestRectArea;
+	private int largestRectNum;
+	private double targetWidthInches;
 	private double[] rectWidth, rectHeight, rectCenterX, rectCenterY, rectArea;
 	private double targetDistance;
-	
+
 	private boolean[] btn, btnPrev;
 
 	private final double HFOV = 29.5;
-	
-	//private Joystick gamepad;
-	
+
+	private RobotDrive drive;
+	private Joystick gamepad, leftStick, rightStick;
+	private Talon leftTalon, rightTalon;
+
+	// private Joystick gamepad;
+
 	public void robotInit() {
 		nt = NetworkTable.getTable("GRIP/myContoursReport");
-		//gamepad = new Joystick(Ports.USB.GAMEPAD);
+
+		leftTalon = new Talon(Ports.PWM.LEFT_TALON);
+		rightTalon = new Talon(Ports.PWM.RIGHT_TALON);
+		leftStick = new Joystick(Ports.USB.LEFT_STICK);
+		rightStick = new Joystick(Ports.USB.RIGHT_STICK);
+		gamepad = new Joystick(Ports.USB.GAMEPAD);
+		drive = new RobotDrive(Ports.PWM.LEFT_TALON, Ports.PWM.RIGHT_TALON);
 	}
 
 	public void autonomousPeriodic() {
@@ -36,19 +50,19 @@ public class Robot extends IterativeRobot {
 		// Get values from NetworkTable and put into SmartDash
 		getNTInfo();
 		debug();
-		
-		/*btn = new boolean[11];
-		for (int i = 1; i <= 10; i++) {
-			//btn[i] = gamepad.getRawButton(i);
-		}*/
-		
-		//btnPrev = Arrays.copyOf(btn, 11);
+
+		/*
+		 * btn = new boolean[11]; for (int i = 1; i <= 10; i++) { //btn[i] =
+		 * gamepad.getRawButton(i); }
+		 */
+
+		// btnPrev = Arrays.copyOf(btn, 11);
 	}
 
 	public boolean button(int i) {
 		return btn[i] && !btnPrev[i];
 	}
-	
+
 	public void testPeriodic() {
 
 	}
@@ -57,36 +71,52 @@ public class Robot extends IterativeRobot {
 	 * <pre>
 	 * private void getNTInfo()
 	 * </pre>
-	 * Gets data from the NetworkTable, then
-	 * calculates distance based on the rectangle and camera's
-	 * horizontal FOV.
+	 * 
+	 * Gets data from the NetworkTable, then calculates distance based on the
+	 * rectangle and camera's horizontal FOV.
 	 */
+
 	private void getNTInfo() {
 		double[] def = {};
-		
+
 		// Get data from NetworkTable
 		rectArea = nt.getNumberArray("area", def);
 		rectWidth = nt.getNumberArray("width", def);
 		rectHeight = nt.getNumberArray("height", def);
 		rectCenterX = nt.getNumberArray("centerX", def);
 		rectCenterY = nt.getNumberArray("centerY", def);
-		
-		// Calculate distance based off of rectangle width and horizontal FOV of camera in feet.
-		// NOTES:
-		//     > Between .25 and .5 ft. off of actual distance
+
+		// The try catch is just in case there is no rectangle visible
 		try {
-			targetDistance = (20.0 / 12.0) * (480.0 / rectWidth[0]) / 2.0 / Math.tan(Math.toRadians(HFOV)); 
+			// Find the largest target rectangle
+			largestRectArea = rectArea[0];
+			largestRectNum = 0;
+
+			for (int i = 0; i < rectArea.length; i++) {
+				if (rectArea[i] >= largestRectArea) {
+					largestRectNum = i;
+				}
+
+			}
+			targetWidthInches = rectWidth[largestRectNum] * .80 * (14.0 / rectHeight[largestRectNum]);
+			// Calculate distance based off of rectangle width and horizontal
+			// FOV of
+			// camera in feet.
+			// NOTES:
+			// > Between .25 and .5 ft. off of actual distance
+			targetDistance = (20.0 / 12.0) * (480.0 / rectWidth[largestRectNum]) / 2.0 / Math.tan(Math.toRadians(HFOV));
 		} catch (Exception e) {
-			targetDistance = Double.NEGATIVE_INFINITY;				
-		}															
-		
+			targetDistance = Double.NEGATIVE_INFINITY;
+		}
+
 	}
-	
+
 	/**
 	 * <pre>
 	 * private void debug()
 	 * </pre>
-	 * Puts data onto the SmartDashboard.
+	 * 
+	 * Puts information onto the SmartDashboard.
 	 */
 	private void debug() {
 		SmartDashboard.putString("Area:", Arrays.toString(rectArea));
@@ -95,6 +125,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putString("Center X:", Arrays.toString(rectCenterX));
 		SmartDashboard.putString("Center Y:", Arrays.toString(rectCenterY));
 		SmartDashboard.putString("Distance: ", "" + targetDistance + " ft.");
+		SmartDashboard.putString("Target Width Inches", "" + targetWidthInches);
 	}
 
 }
