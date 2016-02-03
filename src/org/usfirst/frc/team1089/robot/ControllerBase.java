9cbe46881f9844258d86bba7486923f56772ea96
Dhruv Samdani <dsam7@users.noreply.github.com>
2016-02-02 16:43:12 -0800

    Creating a conflict

--- src/org/usfirst/frc/team1089/robot/ControllerBase.java
+++ src/org/usfirst/frc/team1089/robot/ControllerBase.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 /**
  * The {@code ControllerBase} class handles all input coming from the 
  * gamepad, left joystick, and right joystick. This has various methods
+ * Alerternate comment
  * to get input, buttons, etc.
  */
 public class ControllerBase {
