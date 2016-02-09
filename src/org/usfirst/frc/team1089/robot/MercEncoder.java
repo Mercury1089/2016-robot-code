ac97ac0eed03eef7cdbc671276f33ade9fa9913a
Gabriel <na>
2016-02-08 22:06:46 -0500

    added a placeholder for GEAR_RATIO in MercEncoder.java - update with proper value (ask value from Mr P)

--- src/org/usfirst/frc/team1089/robot/MercEncoder.java
+++ src/org/usfirst/frc/team1089/robot/MercEncoder.java
@@ -3,7 +3,8 @@ package org.usfirst.frc.team1089.robot;
 public class MercEncoder {
 	public final static double WHEEL_SIZE_INCHES = 4.0;
 	public final static double TICKS_PER_ROTATION = 1440;
-	public final static double DISTANCE_PER_TICK_INCHES = (WHEEL_SIZE_INCHES * Math.PI / TICKS_PER_ROTATION);
+	public final static double GEAR_RATIO = 1.0; // TODO FIX THIS (on new robot should be a number between 0.0 and 1.0)
+	public final static double DISTANCE_PER_TICK_INCHES = (WHEEL_SIZE_INCHES * GEAR_RATIO * Math.PI / TICKS_PER_ROTATION);
 	public final static double DISTANCE_PER_TICK_FEET = DISTANCE_PER_TICK_INCHES / 12;
 	
 	public static double distanceTravelled(double count, double sign){
