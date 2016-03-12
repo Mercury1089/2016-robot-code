e666d3909f3022e542fa7d8137395fed6ac9e5a9
dsam7 <dhruvsamdani@gmail.com>
2016-03-12 12:58:06 -0800

    working logger text file accessible thru winSCP

--- src/org/usfirst/frc/team1089/robot/NTListener.java
+++ src/org/usfirst/frc/team1089/robot/NTListener.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team1089.robot;
 
+import java.util.Arrays;
+
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
@@ -19,15 +21,15 @@ public class NTListener implements ITableListener{
 		
 		t.addTableListener(this);
 		
-		Logger.log("Area: " + t.getNumberArray("area", def), " Width: " + t.getNumberArray("width", def), 
-				   " Height: " + t.getNumberArray("height", def), " CenterX: " + t.getNumberArray("centerX", def), 
-				   "CenterY: " + t.getNumberArray("centerY", def));
+		Logger.log("Area: " + Arrays.toString(t.getNumberArray("area", def)), " Width: " + Arrays.toString(t.getNumberArray("width", def)), 
+				   " Height: " + Arrays.toString(t.getNumberArray("height", def)), " CenterX: " + Arrays.toString(t.getNumberArray("centerX", def)), 
+				   "CenterY: " + Arrays.toString(t.getNumberArray("centerY", def)));
 		
 	}
 	
 	@Override
 	public void valueChanged(ITable source, String string , Object o, boolean bln){
-		Logger.log("String: " + string + " Value: " + o + " new: " + bln);
+		Logger.log("String: " + string + " Value: " + Arrays.toString((double[])o) + " new: " + bln);
 	}
 
 	public void stop() {
