408a0b4a85851e7772bcfe7932dd658173864c68
Gabriel <na>
2016-03-24 22:28:45 -0400

    Redid all the measurements. Cut a foot of travel for every position.
    (because in practice we start only about three feet from the defense
    rather than the four we had estimated possible at one point thanks in
    part to the bumper)
    
    Cut an extra foot on top of that for P2 and P4 (because it was not quite
    needed)
    
    Everything needs to be re-tested!

--- src/org/usfirst/frc/team1089/auton/Defense.java
+++ src/org/usfirst/frc/team1089/auton/Defense.java
@@ -10,12 +10,16 @@ public class Defense{
 	private Shooter shooter;
 	
 	public static final int 
-					BEFORE_DEFENSE_FEET = 4, THROUGH_DEFENSE_FEET = 4,
-					INITIAL_CDF_FEET = 4, REMAINING_CDF_FEET = 4,
-					AFTER_DEFENSE_FEET = 3, BUFFER_DEFENSE_FEET = 1, 
-					ROCK_WALL_SLIPPAGE_COMPENSATION_FEET = 2,
-					THROUGH_DEFENSE_FEET_ROCK_WALL =  THROUGH_DEFENSE_FEET + ROCK_WALL_SLIPPAGE_COMPENSATION_FEET,  // for rockwall we add 2 feet to allow for huge slippage + buffer 
-					BUFFER_AFTER_DEFENSE_FEET = 1; // this is a variable buffer
+	
+					BEFORE_DEFENSE_FEET = 3, // the robot is 38-inch long with the bumper. With 2 inches above the line we are 3 feet away from defense.
+					THROUGH_DEFENSE_FEET = 4, // defenses are 4 feet by definition
+					INITIAL_CDF_FEET = 3, // cheval de frise
+					REMAINING_CDF_FEET = 4, // cheval de frise
+					CORE_AFTER_DEFENSE = 3, // what we need to clear the defense (so the back of the robot goes back the defense)
+					BUFFER_AFTER_DEFENSE = 2, // a buffer to avoid not fully clearing the defense
+					AFTER_DEFENSE_FEET = CORE_AFTER_DEFENSE + BUFFER_AFTER_DEFENSE, // this corresponds to 3 feet to clear plus 2 feet of buffer
+					ROCK_WALL_SLIPPAGE_COMPENSATION_FEET = 2, // should be adjusted so that back is about two feet (or rather BUFFER_AFTER_DEFENSE) away from defense after breaching it
+					THROUGH_DEFENSE_FEET_ROCK_WALL =  THROUGH_DEFENSE_FEET + ROCK_WALL_SLIPPAGE_COMPENSATION_FEET;  // for rockwall we add 2 feet to allow for huge slippage + buffer 
 	
 	public Defense(DriveTrain d, Shooter s, DefenseEnum dE) {
 		drive = d;
@@ -28,7 +32,7 @@ public class Defense{
 			case LOW_BAR: {
 				Logger.log("Defense LOW_BAR in");
 				shooter.raise(Shooter.DOWN);
-				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5);
+				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5);
 				drive.waitMove(); 		//moveDistance is an asynchronous operation - we need to wait until it is done
 				Logger.log("Defense LOW_BAR out");
 				break;
@@ -36,7 +40,7 @@ public class Defense{
 			case MOAT: {
 				Logger.log("Defense MOAT in");
 				shooter.raise(Shooter.LOW);
-				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 12.0); //TODO test and change ALL these values
+				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 12.0); //TODO test and change ALL these values
 				drive.waitMove();		//full speed
 				Logger.log("Defense MOAT out");
 				break;
@@ -44,7 +48,7 @@ public class Defense{
 			case ROUGH_TERRAIN: {
 				Logger.log("Defense ROUGH_TERRAIN in");
 				shooter.raise(Shooter.LOW);
-				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 8.0); 
+				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 8.0); 
 				drive.waitMove();		//need fast PID voltage - to be changed
 				Logger.log("Defense ROUGH_TERRAIN out");
 				break;
@@ -52,7 +56,7 @@ public class Defense{
 			case RAMPARTS: {
 				Logger.log("Defense RAMPARTS in");
 				shooter.raise(Shooter.LOW);
-				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 6); 
+				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 6); 
 				drive.waitMove();		//human speed
 				Logger.log("Defense RAMPARTS out");
 				break;
@@ -60,7 +64,7 @@ public class Defense{
 			case ROCK_WALL: {
 				Logger.log("Defense ROCK_WALL in");
 				shooter.raise(Shooter.MEDIUM);
-				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET_ROCK_WALL + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 8.0); 
+				drive.moveDistance(BEFORE_DEFENSE_FEET + THROUGH_DEFENSE_FEET_ROCK_WALL + AFTER_DEFENSE_FEET, 0.4, 0, 0, 8.0); 
 				drive.waitMove();		// full speed
 				Logger.log("Defense ROCK_WALL out");
 				break;
@@ -71,7 +75,7 @@ public class Defense{
 				drive.moveDistance(INITIAL_CDF_FEET, 0.4, 0, 0, 4.5);
 				drive.waitMove();
 				shooter.raise(Shooter.DOWN);
-				drive.moveDistance(REMAINING_CDF_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5); 
+				drive.moveDistance(REMAINING_CDF_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5); 
 				drive.waitMove();
 				Logger.log("Defense CHEVAL_DE_FRISE out");
 				break;
@@ -82,7 +86,7 @@ public class Defense{
 				drive.moveDistance(INITIAL_CDF_FEET, 0.4, 0, 0, 4.5);
 				drive.waitMove();
 				//OPEN PORTCULLIS DOOR code
-				drive.moveDistance(REMAINING_CDF_FEET + AFTER_DEFENSE_FEET + BUFFER_AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5);	//change constants 
+				drive.moveDistance(REMAINING_CDF_FEET + AFTER_DEFENSE_FEET, 0.4, 0, 0, 4.5);	//change constants 
 				drive.waitMove();
 				Logger.log("Defense PORTCULLIS out");
 				break;
