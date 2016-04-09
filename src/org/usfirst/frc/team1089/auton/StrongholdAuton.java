54e570edfc27bc191fc75fb1839f819f1e455639
Mercury 1089 <Mercury 1089@10.50.32.172>
2016-04-09 16:54:52 -0700

    shoot distance to 10ft

--- src/org/usfirst/frc/team1089/auton/StrongholdAuton.java
+++ src/org/usfirst/frc/team1089/auton/StrongholdAuton.java
@@ -60,9 +60,9 @@ public class StrongholdAuton {
 						ROTATE_POST_DEFENSE_P5_DEGREES_SECRET_PATH = -60;
 	
 	// maximum optimal shooting distances
-	private static double SHOOT_DISTANCE_P1_P3_P4_FEET = 11, // maximum distance for long shot (farthest optimal distance) - need to be within shooting range!
+	private static double SHOOT_DISTANCE_P1_P3_P4_FEET = 10, // maximum distance for long shot (farthest optimal distance) - need to be within shooting range!
 					SHOOT_DISTANCE_P2_FEET_LEFT_PATH = 5,	// shooting from left, short shot
-					SHOOT_DISTANCE_P2_FEET_CENTER_PATH = 11,	//shooting from center, long shot 
+					SHOOT_DISTANCE_P2_FEET_CENTER_PATH = 10,	//shooting from center, long shot 
 					/*SHOOT_DISTANCE_P3_FEET = 11,
 					SHOOT_DISTANCE_P4_FEET = 11,*/ 
 					SHOOT_DISTANCE_P5_FEET_RIGHT_PATH = 5, // maximum distance for short shot (farthest optimal distance) - need to be within shooting range!
